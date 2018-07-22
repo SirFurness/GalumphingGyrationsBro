41746e5c6a0fbe7c04134b38da806fad9efb9bba
SirFurness <eric.1334@icloud.com>
2018-07-21 21:39:51 -0700

    Random pulling

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import random
+
 adjectives = ["Really"]
 noun = []
 noun2 = []
 
+def get(array):
+    return array[random.randint(0, len(array))]
+
 if __name__ == "__main__":
-    print("hi")
+    print(get(adjectives) + " " + get(noun) + " " + get(noun2))
